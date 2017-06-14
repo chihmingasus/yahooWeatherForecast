7d7e689e162fb4c09c0ed82f9701157409f1273e
Chihming Chuang <chihming>
2017-06-14 17:49:34 +0800

    modify degree unit text position

--- app.py
+++ app.py
@@ -91,7 +91,7 @@ def makeWebhookResult(data):
 
     speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
              ", the temperature is " + condition.get('temp') + " " + units.get('temperature') + \
-			 "humidity is " + atmosphere.get('humidity')		
+			 ", humidity is " + atmosphere.get('humidity')
 
     print("Response:")
     print(speech)
